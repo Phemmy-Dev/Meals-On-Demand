da7f0e7f22ce5c20b38c186050dcef8756d89c7a
Phemmy <phemmy123x@gmail.com>
2022-10-10 02:25:44 +0100

    Meals On Demand First Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementById("nav-items");
+
+    function showMenu() {
+            navLinks.style.right = "0"
+    }
+    
+    
+    function hideMenu() {
+            navLinks.style.right = "-30%"
+    } 
